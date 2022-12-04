2be2d3eb111be750124246b4d3a82ff6d5e0baa3
jhodk <john_hodkinson@hotmail.co.uk>
2022-12-04 21:57:27 +0000

    Day 4 part 1 and 2

---
+++ Day 4/assignments.py
@@ -0,0 +1,20 @@
+import os
+file = open(os.path.join(os.path.dirname(__file__), "input.txt"))
+
+def processLine(line):
+  ranges = list(map(lambda a: a.split("-"), line.split(",")))
+  ranges = list(map(lambda a: {*range(int(a[0]),int(a[1])+1)}, ranges))
+  if len(ranges[0].intersection(ranges[1])) >= min(len(ranges[0]),len(ranges[1])):
+    return 1
+  else:
+    return 0
+
+totalContained = 0
+while True:
+    line = file.readline()
+    if line != "":
+      totalContained += processLine(line)
+    else:
+      break
+
+print("Total contained:",totalContained)
