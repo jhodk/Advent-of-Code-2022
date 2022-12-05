8915b5b7788c58b7c126b8a195e1811c43e8e991
jhodk <john_hodkinson@hotmail.co.uk>
2022-12-05 18:56:32 +0000

    Day 5 part 1

---
+++ Day 5/crane.py
@@ -0,0 +1,39 @@
+import os
+file = open(os.path.join(os.path.dirname(__file__), "input.txt"))
+
+from collections import deque
+
+startCraning = False
+stacks = []
+
+def processLine(line):
+  if startCraning:
+    processInstruction(line)
+  else:
+    processConfiguration(line)
+
+def processConfiguration(line):
+  global stacks
+  if stacks == []:
+    stacks = [deque() for i in range(len(line)//4)]
+  for i in range(len(stacks)):
+    if line[1 + i*4] != " ":
+      stacks[i].appendleft(line[1 + i*4])
+
+def processInstruction(line):
+  line = list(map(int, line.split()[1::2]))
+  for i in range(line[0]):
+    stacks[line[2]-1].append(stacks[line[1]-1].pop())
+
+while True:
+    line = file.readline()
+    if line != "":
+      if line[0] == " ":
+        startCraning = True
+        file.readline()
+        line = file.readline()
+      processLine(line)
+    else:
+      break
+
+print("Top of stacks:", "".join(list(map(lambda a: a.pop(), stacks))))
