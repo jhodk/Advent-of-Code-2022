0e89b7cb37d24c4abba99764ea96dfc0ee7027b5
jhodk <john_hodkinson@hotmail.co.uk>
2022-12-10 18:07:30 +0000

    Day 7 both

---
+++ Day 7/directory.py
@@ -0,0 +1,35 @@
+import os
+file = open(os.path.join(os.path.dirname(__file__), "input.txt"))
+
+working_directory_path = []
+sizes_map = {"/":0}
+
+def processLine(line):
+  args = line.split()
+  if args[0] == "$":
+    if args[1] == "cd":
+      if args[2] == "..":
+        working_directory_path.pop()
+      else:
+        working_directory_path.append(args[2])
+  else:
+    if args[0] != "dir":
+      working_directory_path_copy = list(working_directory_path)
+      while(len(working_directory_path_copy) > 0):
+        sizes_map["/".join(working_directory_path_copy)] = sizes_map.get("/".join(working_directory_path_copy), 0) + int(args[0])
+        working_directory_path_copy.pop()
+
+while True:
+    line = file.readline()
+    if line != "":
+      processLine(line)
+    else:
+      break
+
+total = 0
+maxSize = 1e5
+for dirSize in sizes_map.values():
+  if(dirSize <= maxSize):
+    total += dirSize
+
+print("Total of all folder sizes <=", maxSize, "is", total)
